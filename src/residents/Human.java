590929d14d4b1774b4ada5e22435110e5dd20460
LakeLove <lakegreene@yahoo.com>
2020-05-19 23:05:11 -0400

    Added abstract methods to Sleeper interface and gave its implementation basic functions.

--- src/residents/Human.java
+++ src/residents/Human.java
@@ -1,6 +1,10 @@
 package residents;
 public class Human extends Resident {
-
-	public Human() {
+	public Human() {}
+	public void sleep() {
+		System.out.println("Time to sleep.");
+	}
+	public void nap() {
+		System.out.println("Time to take a nap.");
 	}
 }
